d9892ef5e087df2728126db8dd8c4c9376519da5
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-07-30 15:30:46 +0700

    Sign of Integer Number

---
+++ GCD0904FunctionExercises/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace GCD0904FunctionExercises
+{
+	class Program
+	{
+		// Sign of Integer Number
+		static void PrintSign(int number)
+		{
+			if (number > 0)
+			{
+				Console.WriteLine($"The number {number} is positive");
+			}
+			else if (number < 0)
+			{
+				Console.WriteLine($"The number {number} is negative");
+			}
+			else
+			{
+				Console.WriteLine($"The number {0} is zero");
+			}
+		}
+		static void Main(string[] args)
+		{
+			//	Sign of integer number
+			PrintSign(10);
+			PrintSign(-10);
+			PrintSign(0);
+
+
+
+			Console.ReadLine();
+		}
+	}
+}
