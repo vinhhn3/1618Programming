b1d8046cdf44dd6bd238b33247a669b68e263e37
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-08-03 13:31:05 +0700

    Add ClassesAndObjects

---
+++ GCD0904ClassesAndObjects/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace GCD0904ClassesAndObjects
+{
+
+
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Delcare an object
+			// ClassName variableName = new ClassName();
+			User firstUser = new User();
+
+			// Assign values to each property of firstUser object
+			firstUser.FullName = "Vinh Hoang";
+			firstUser.Age = 30;
+			firstUser.Address = "Le Loi";
+			firstUser.Id = 123;
+
+			// Read all properties of firstUser
+			Console.WriteLine("Full Name: " + firstUser.FullName);
+			Console.WriteLine("Age: " + firstUser.Age);
+			Console.WriteLine("Address: " + firstUser.Address);
+			Console.WriteLine("Id: " + firstUser.Id);
+
+			// Let object use function
+			firstUser.Run();
+			firstUser.Sleep();
+
+
+
+			Console.ReadLine();
+		}
+	}
+}
---
+++ GCD0904ClassesAndObjects/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GCD0904ClassesAndObjects
+{
+	class User
+	{
+		// Properties
+		public string FullName;
+		public int Age;
+		public string Address;
+		public int Id;
+
+		// Actions
+		public void Talk()
+		{
+			Console.WriteLine("User is talking ...");
+		}
+
+		public void Run()
+		{
+			Console.WriteLine("User is running ...");
+		}
+
+		public void Eat()
+		{
+			Console.WriteLine("User is eating ...");
+		}
+
+		public void Sleep()
+		{
+			Console.WriteLine("User is sleeping ...");
+		}
+	}
+}
