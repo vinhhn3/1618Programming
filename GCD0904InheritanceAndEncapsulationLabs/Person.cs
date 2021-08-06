55fe6286158b1d8b25a991cd5fe5ccf3ca6677f0
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-08-06 15:43:22 +0700

    Sort Persons by Name and Age

---
+++ GCD0904InheritanceAndEncapsulationLabs/Person.cs
@@ -0,0 +1,27 @@
+﻿namespace GCD0904InheritanceAndEncapsulationLabs
+{
+	class Person
+	{
+		public string FirstName;
+		public string LastName;
+		public int Age;
+
+		public Person()
+		{
+
+		}
+
+		public Person(string firstName, string lastName, int age)
+		{
+			FirstName = firstName;
+			LastName = lastName;
+			Age = age;
+		}
+
+		public override string ToString()
+		{
+			string result = $"{FirstName} {LastName} is {Age} years old";
+			return result;
+		}
+	}
+}
--- GCD0904InheritanceAndEncapsulationLabs/Program.cs
+++ GCD0904InheritanceAndEncapsulationLabs/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GCD0904InheritanceAndEncapsulationLabs
 {
@@ -20,6 +22,27 @@ namespace GCD0904InheritanceAndEncapsulationLabs
 			cat.Eat();      // class Animal
 			cat.Meow();     // class Cat
 
+
+			Console.ReadLine();
+
+			int lines = int.Parse(Console.ReadLine());  // Enter number of persons
+			List<Person> persons = new List<Person>();  // Create list of Person
+			for (int i = 0; i < lines; i++)             // Execute loops
+			{
+				string[] cmdArgs = Console.ReadLine().Split().ToArray();  // Process string
+				Person person = new Person(
+					cmdArgs[0], // FullName
+					cmdArgs[1], // LastName
+					int.Parse(cmdArgs[2])); // Age
+
+				persons.Add(person);      // Add new person in to List
+			}
+
+			persons.OrderBy(p => p.FirstName) // Order by FirstName
+						 .ThenBy(p => p.Age)        // Order by Age
+						 .ToList()                  // Convert result => List
+						 .ForEach(p => Console.WriteLine(p.ToString()));
+			// foreach item in List, print info
 
 			Console.ReadLine();
 		}
