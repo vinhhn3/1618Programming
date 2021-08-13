9d3ee3769b3096048911156d56dc6649cd0b7546
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-08-13 15:13:53 +0700

    Overloading methods

---
+++ GCD0904Polymorphism/Animal.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace GCD0904Polymorphism
+{
+	class Animal
+	{
+		public void Eat()
+		{
+			Console.WriteLine("Animal is eating ...");
+		}
+
+		public string Eat(string food)
+		{
+			string result = $"Animal eats {food}";
+			return result;
+		}
+
+		public string Eat(string food, string drink)
+		{
+			string result = $"Animal eats {food} and drink {drink}";
+			return result;
+		}
+	}
+}
---
+++ GCD0904Polymorphism/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace GCD0904Polymorphism
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Overloading methods
+			// Same function name but behave differently
+			Animal animal = new Animal();
+			animal.Eat(); // void
+
+			Console.WriteLine(animal.Eat("meat")); // return string
+
+			Console.WriteLine(animal.Eat("meat", "water")); // string
+
+			Console.ReadLine();
+		}
+	}
+}
